655e858a10530b49d2669864ecaf6304628bf6b0
Alexander Schmidtberger <99815034+Kingsmann007@users.noreply.github.com>
2023-05-17 13:29:15 +0000

    17.5.23

--- Hospital/Hospital.java
+++ Hospital/Hospital.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Arrays;
 import java.util.Comparator;
 public class Hospital{
@@ -35,8 +36,8 @@ public Hospital(String name,Address address){
     }
     
   }
-  public void showPatients(){
-    Arrays.sort(patients, comparators[0]);
+  public void showPatients(int j){
+    Arrays.sort(patients, comparators[j]);
     for(int i=0;i<patients.length;i++){
       if(patients[i]!=null){
         System.out.println(patients[i]);
@@ -47,7 +48,7 @@ public Hospital(String name,Address address){
     comparators[0] = new Comparator<Patient>(){
       public int compare(Patient o, Patient p){
        if(o==null){
-         if(p != null) return 1;
+         if(p != null) return -1;
          return 0;
        }
        if(p==null){
@@ -64,16 +65,53 @@ public Hospital(String name,Address address){
          return o.getLName().compareTo(p.getLName());
        } 
      };
-  }
+    comparators[1] = new Comparator<Patient>(){
+      public int compare(Patient o, Patient p){
+       if(o==null){
+         if(p != null) return -1;
+         return 0;
+       }
+       if(p==null){
+         if(o != null) return 1;
+       }
+        int date = o.getAdmDate().compareTo(p.getAdmDate());
+        if(date ==0){
+          if(o.getSex()==p.getSex()) return 0;
+          if(o.getSex()<p.getSex()) return -1;
+          return 1;
+        }
+        return date;
+      }
+     };
+    comparators[2] = new Comparator<Patient>(){
+      public int compare(Patient o, Patient p){
+       if(o==null){
+         if(p != null) return -1;
+         return 0;
+        }
+        if(p==null){
+          if(o != null) return 1;
+        }
+        return Integer.valueOf(o.getAge()).compareTo(p.getAge());
+      }
+    };
+    comparators[3] = new Comparator<Patient>(){
+      public int compare(Patient o, Patient p){
+       if(o!=null && p==null) return 1;
+       if(o==null && p!=null) return -1;
+       return 0;
+      }
+    };
+  }  
   public static void main(String[] args) {
     Address a =new Address("a","a","a","a");
     Hospital h=new Hospital("lol",a);
-    h.addPatient(new Patient("k","jw",5,'m'));
-    h.addPatient(new Patient("s","jf",4,'m'));
-    h.addPatient(new Patient("g","jh",3,'m'));
-    h.addPatient(new Patient("a","jj",3,'m'));
-    h.addPatient(new Patient("a","jj",3,'m'));
+    h.addPatient(new Patient("k","jw",5,'e'));
+    h.addPatient(new Patient("s","jf",4,'d'));
+    h.addPatient(new Patient("g","jh",3,'c'));
+    h.addPatient(new Patient("a","jj",3,'b'));
+    h.addPatient(new Patient("a","jj",3,'a'));
     
-    h.showPatients();
+    h.showPatients(1);
   }   
 }
